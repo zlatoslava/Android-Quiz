eca16c1dc13b46896108edc20bd7349cf143ccb8
zlatoslava <anckamur@gmail.com>
2020-09-19 19:20:57 +0200

    Create model for category, add some constants

--- app/src/main/java/savinykh/zlatoslava/Constants/AppConstants.java
+++ app/src/main/java/savinykh/zlatoslava/Constants/AppConstants.java
@@ -4,4 +4,10 @@ public class AppConstants {
     public static final String BUNDLE_KEY_TITLE = "title";
     public static final String BUNDLE_KEY_URL = "url";
     public static final long SITE_CASHE_SIZE = 10 * 1024 * 1024;
+
+    // category file
+    public static final String CONTENT_FILE = "json/categories/quiz_category.json";
+    public static final String JSON_KEY_ITEMS = "items";
+    public static final String JSON_KEY_CATEGORY_ID = "question_category";
+    public static final String JSON_KEY_CATEGORY_NAME = "category_name";
 }
